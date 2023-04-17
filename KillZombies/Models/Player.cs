eca0d5f73fabb1ef9fc11ec334efa4a1cc8a5910
GlebMozglyakov <83268358+GlebMozglyakov@users.noreply.github.com>
2023-04-17 21:08:07 +0500

    Add files via upload

---
+++ KillZombies/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace KillZombies.Models
+{
+    class Player
+    {
+        public int Health { get; set; }
+
+        public int Armor { get; set; }
+        public Point currentPosition { get; }
+
+        public Player()
+        {
+
+        }
+    }
+}
