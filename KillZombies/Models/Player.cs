515c1fa329c87251daa6718bd3dc3e4178c2a6b1
GlebMozglyakov <83268358+GlebMozglyakov@users.noreply.github.com>
2023-04-18 20:28:28 +0500

    Add files via upload

--- KillZombies/Models/Player.cs
+++ KillZombies/Models/Player.cs
@@ -1,15 +1,67 @@
-﻿namespace KillZombies.Models
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KillZombies.Models
 {
     class Player
     {
+        public Texture2D Texture { get; set; }
+
         public int Health { get; set; }
 
         public int Armor { get; set; }
-        public Point currentPosition { get; }
+
+        public Point CurrentPosition { get; }
+
+        public MoveDirection Direction { get; set; }
+
+        private Rectangle rectangle;
+
+        public Rectangle Rectangle { 
+            get 
+            { 
+                return rectangle; 
+            } 
+            set 
+            { 
+                rectangle = value;
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return rectangle.X;
+            }
+            set
+            {
+                rectangle.X = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return rectangle.Y;
+            }
+            set
+            {
+                rectangle.Y = value;
+            }
+        }
 
         public Player()
         {
 
         }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, rectangle, Color.White);
+        }
+
+
     }
 }
